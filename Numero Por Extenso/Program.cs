48921b025983202445be535fb2876402ef3f3fb8
Thiago Henriques Pessoa <thiagokxl5@gmail.com>
2022-04-28 08:30:30 -0300

    Adicionar arquivos de projeto.

---
+++ Numero Por Extenso/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConverteNumero;
+using System;
+
+namespace Numero_Por_Extenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número:");
+            var numero = Convert.ToDecimal(Console.ReadLine());
+            string extenso = Conversor.EscreverExtenso(numero);
+            Console.WriteLine(extenso);
+
+        }
+    }
+}
